74b5eb7ff94acb7352774cc6dc1c1bafb95f4dca
Cade Fowler <bcfowler2@catamount.wcu.edu>
2024-06-04 03:13:50 -0400

    added spotify api and created get_song function

--- backend.py
+++ backend.py
@@ -1,4 +1,39 @@
-import pymongo
+import pymongo, requests, random, string, spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = "user-read-currently-playing"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id='bb2d26a61558491992f8fb35abd02168', client_secret='50056d0173c64710b76a7102888cea92', redirect_uri='http://localhost:8888/callback'))
+
+ENTRIES_TO_REMOVE = (
+    'available_markets',
+    'disc_number', 
+    'duration_ms', 
+    'explicit', 
+    'external_ids',
+    'href',
+    'is_local',
+    'uri',
+    'type',
+    'track_number',
+    'preview_url'
+)
+
+ALBUM_ENTRIES_TO_REMOVE = (
+    'href',
+    'available_markets',
+    'id',
+    'uri',
+    'images',
+    'release_date_precision',
+    'total_tracks',
+    'artists',
+    'type'
+)
+
+ARTISTS_ENTRIES_TO_REMOVE = (
+    'href',
+    'uri'
+)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 colordb = myclient["colordatabase"]
@@ -6,18 +41,29 @@ songs = colordb["songs"]
 
 song1 = { "name": "xx", "artist": "", "colors": ["red", "blue"]}
 
-x = songs.insert_one(song1)
-print(x.inserted_id)
-
-def get_song_info():
+def insert_song(song):
     pass
+    # songs.update_one(song, {$push: {colors: song.colors} }, {upsert: true})
 
-def input_color(color):
+def insert_color(color):
     pass
 
 def get_song_colors(song_name):
     colors = songs.find({"name": song_name}, {"colors": 1})
+    for y in colors:
+        print(y)
 
-get_song_colors("xx")
+def get_song():
+    song_id = sp.currently_playing()['item']['id']
+    song = sp.track(song_id)
+    [song.pop(key) for key in ENTRIES_TO_REMOVE]
+    [song['album'].pop(key) for key in ALBUM_ENTRIES_TO_REMOVE]
+    for artist in song['artists']:
+        [artist.pop(key) for key in ARTISTS_ENTRIES_TO_REMOVE]
+    return song
+
+print(get_song())
+
+
 
 
