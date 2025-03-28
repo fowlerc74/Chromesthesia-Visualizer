5243a271d4e8a947e51b20cf1d6480533ab9d03e
Cade Fowler <bcfowler2@catamount.wcu.edu>
2025-03-27 20:35:20 -0400

    Changed _id to spotify id

--- backend/routes/songs.mjs
+++ backend/routes/songs.mjs
@@ -9,28 +9,41 @@ router.get("/:id", async (req, res) => {
     console.log(req.params.id);
     // TODO Validate id
 
-    let query = {_id: new ObjectId(req.params.id)};
+    let query = {id: req.params.id};
     let result = await db.collection("songs").findOne(query);
   
     if (!result) res.send("Not found").status(404);
     else res.send(result).status(200);
 });
 
-// Add new color to song
-router.post("/:id", async (req, res) => {
-    let query = {_id: new ObjectId(req.params.id)};
-    let song = await db.collection("songs").findOne(query);
-    let result = song.colors;
+// Add new song
+router.post("/", async (req, res) => {
+    // const query = {id: req.params.id};
+
+    let result = await db.collection("songs").findOne(req.body.id);
+    console.log(req.body.id)
   
     if (!result) res.send("Not found").status(404);
     else res.send(result).status(200);
+
 });
+
+// // Add new colors to song
+// router.post("/", async (req, res) => {
+//     const query = {id: req.params.id};
+
+//     let song = await db.collection("songs").insertOne(req.body);
+//     let result = song.colors;
+  
+//     if (!result) res.send("Not found").status(404);
+//     else res.send(result).status(200);
+
+// });
 
 // Get song colors
 router.get("/:id/colors/", async (req, res) => {
     let query = {_id: new ObjectId(req.params.id)};
     let song = await db.collection("songs").findOne(query);
-    console.log(song);
     let result = song.colors;
   
     if (!result) res.send("Not found").status(404);
@@ -41,11 +54,9 @@ router.get("/:id/colors/", async (req, res) => {
 // TODO Make this better
 router.get("/", async (req, res) => {
     const filters = req.query;
-    console.log(filters);
     let results = await db.collection("songs").find(filters)
       .limit(50)
       .toArray();
-    console.log(results.length);
     res.send(results).status(200);
 });
 
