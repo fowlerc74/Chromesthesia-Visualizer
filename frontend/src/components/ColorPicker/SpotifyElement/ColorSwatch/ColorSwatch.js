559a4a4e6078cbd5126aaac057e8b309673a40f6
Cade Fowler <bcfowler2@catamount.wcu.edu>
2025-04-28 20:04:44 -0400

    finished edit and delete button styling

--- frontend/src/components/ColorPicker/SpotifyElement/ColorSwatch/ColorSwatch.js
+++ frontend/src/components/ColorPicker/SpotifyElement/ColorSwatch/ColorSwatch.js
@@ -1,6 +1,7 @@
 import './ColorSwatch.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrashCan } from '@fortawesome/free-regular-svg-icons'
+import { useState } from 'react'
 
 const ColorSwatch = (props) => {
 
@@ -11,19 +12,59 @@ const ColorSwatch = (props) => {
         return (red + green + blue) / 3 <= 140
     }
 
-    //TODO fix key when duplicate colors
+    const [isHoverEdit, setIsHoverEdit] = useState(false);
+    const [isHoverDelete, setIsHoverDelete] = useState(false);
+
+    const handleMouseEnterEdit = () => {
+        setIsHoverEdit(true);
+    };
+    const handleMouseLeaveEdit = () => {
+        setIsHoverEdit(false);
+    };
+    const handleMouseEnterDelete = () => {
+        setIsHoverDelete(true);
+    };
+    const handleMouseLeaveDelete = () => {
+        setIsHoverDelete(false);
+    };
+
+    const buttonColor = isDark() ? 'rgba(255, 255, 255, 0)' : 'rgba(0, 0, 0, 0)';
+    const buttonHoverColor = isDark() ? 'rgba(255, 255, 255, .2)' : 'rgba(0, 0, 0, .2)';
+
     return (
-        <div className="swatch" style={{'background': props.color}}>
+        <button className="swatch" style={{'background': props.color}} >
             <div className="swatch-text" style={isDark() ? {color: "#f5f5f5"} : {color: "#1c1c1c"}}>
-                <button className="delete swatch-button">
-                    <FontAwesomeIcon icon={faTrashCan} style={isDark() ? {color: "#f5f5f5"} : {color: "#1c1c1c"}}/>
+                <button 
+                    className="edit swatch-button" 
+                    onMouseEnter={handleMouseEnterEdit}
+                    onMouseLeave={handleMouseLeaveEdit}
+                    style={ isHoverEdit ?
+                        {backgroundColor: buttonHoverColor} :
+                        {backgroundColor: buttonColor}
+                    }
+                >
+                    <FontAwesomeIcon 
+                        icon={faPenToSquare} 
+                        style={isDark() ? {color: "#f5f5f5"} : {color: "#1c1c1c"}}
+                    />
                 </button>
-                <button className="edit swatch-button">
-                    <FontAwesomeIcon icon={faPenToSquare} style={isDark() ? {color: "#f5f5f5"} : {color: "#1c1c1c"}}/>
+                <button 
+                    className="delete swatch-button" 
+                    onMouseEnter={handleMouseEnterDelete}
+                    onMouseLeave={handleMouseLeaveDelete}
+                    style={ isHoverDelete ?
+                        {backgroundColor: buttonHoverColor} :
+                        {backgroundColor: buttonColor}
+                    }
+                >
+                    <FontAwesomeIcon 
+                        icon={faTrashCan} 
+                        style={isDark() ? {color: "#f5f5f5"} : {color: "#1c1c1c"}}
+                    />
                 </button>
                 {props.color}
             </div>
-        </div> 
+        </button> 
     )
 }
 
